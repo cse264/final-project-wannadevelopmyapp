fc586e9f68bf97472380980b6cd72d11be856523
Bratislav Petkovic <brp221@lehigh.edu>
2020-12-02 18:34:21 -0500

    added fetching of all users and raw display on home page of trainees and trainers

--- routes/home.js
+++ routes/home.js
@@ -15,7 +15,7 @@ router.get('/', async function(req,res,next){
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL
     },
-    function(accessToken, refreshToken, profile, done) {
+     function(accessToken, refreshToken, profile, done) {
         userProfile=profile;
         console.log(userProfile);
         if(userProfile != undefined){
@@ -48,7 +48,6 @@ router.get('/', async function(req,res,next){
             }
           });
         }
-        
         console.log("THE GOODIES: " , userProfile._json)
         return done(null, userProfile);
     }
@@ -58,7 +57,21 @@ router.get('/', async function(req,res,next){
   //console.log(userProfile._json)
   //console.log(userProfile);
   //users: foundUsers
-  res.render('home', { title: 'home' });
+  const trainer_filter = {UserType : "Trainer"};
+  const trainee_filter = {UserType : "Trainee"};
+  let all_Trainers = await User.find(trainer_filter);
+  let all_Trainees = await User.find(trainee_filter);
+  let trainer_usernames = [];
+  let trainee_usernames = [];
+  for(let i=0; i < all_Trainers.length;i++){
+    trainer_usernames.push(all_Trainers[i].Username)
+  }
+  for(let i=0; i < all_Trainees.length;i++){
+    trainee_usernames.push(all_Trainees[i].Username)
+  }
+  console.log(trainer_usernames);
+  console.log(trainee_usernames);
+  res.render('home', { title: 'home' , trainers: trainer_usernames, trainees: trainee_usernames});
 });
 
 module.exports = router;
